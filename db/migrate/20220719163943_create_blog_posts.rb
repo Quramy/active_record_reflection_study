a49c944de30a0f5c7ba8019973fc0553f9a006d1
Quramy <yosuke.kurami@gmail.com>
2022-07-20 01:49:50 +0900

    add 2 models

---
+++ db/migrate/20220719163943_create_blog_posts.rb
@@ -0,0 +1,9 @@
+class CreateBlogPosts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :blog_posts do |t|
+      t.string :title
+      t.references :user, foreign_key: true
+      t.timestamps
+    end
+  end
+end
